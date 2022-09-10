cb14417ee275867144cd0747e44f28e35a440645
LazyProger643 <tzidan@mail.ru>
2022-09-10 10:30:52 +0500

    Gems class renamed to GemContainer

--- Assets/Scripts/LevelData.cs
+++ Assets/Scripts/LevelData.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class LevelData : MonoBehaviour
 {
     [SerializeField] private Transform _playerStartPosition;
-    [SerializeField] private Gems _gems;
+    [SerializeField] private GemContainer _gems;
 
     public Vector3 PlayerStartPosition => _playerStartPosition.position;
     public int GemCount => _gems.GetComponentsInChildren<Gem>().Length;
